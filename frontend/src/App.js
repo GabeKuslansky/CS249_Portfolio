c0b6083270445c1431de1c07415929efdb6cca07
GabeKuslansky <kuslanskyg@gmail.com>
2018-09-05 12:29:05 -0400

    Implemented react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      homeworks: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Assignment1.jar</p>
+        <p>Assignment2.jar</p>
+        <p>Assignment3.jar</p>
       </div>
     );
   }
